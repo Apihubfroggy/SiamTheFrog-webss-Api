5c107be15487464df578c46f0e197d02f5fb851f
Apihub-SiamTheFrog <froggyapihub@gmail.com>
2024-12-13 10:53:10 +0600

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "screenshot-api",
+  "version": "1.0.0",
+  "main": "index.js",
+  "dependencies": {
+    "axios": "^1.4.0",
+    "express": "^4.18.2"
+  },
+  "scripts": {
+    "start": "node index.js"
+  }
+}
